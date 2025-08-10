85ada016dbff2efed65b8eb01cae5715f571156c
Royagozalpour <Royago79@gmail.com>
2025-08-10 17:15:14 +0330

    Add Lesson 1 to 4: Printing, Input Function, Debugging Practice, and Variables

---
+++ Auditorium/Lesson 2 Day 1 - Input Function.py
@@ -0,0 +1,3 @@
+name = input()
+name2 = len(name)
+print(name2)
---
+++ Auditorium/Lesson 4 Day 1 - Variables.py
@@ -0,0 +1,15 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+temp = a
+a = b
+b = temp
+
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
